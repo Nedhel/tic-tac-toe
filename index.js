9a2bbc5575237b8d7d10d48e3fcec7ab647a5a22
Carlos Zalpa <nedhel.90@gmail.com>
2022-08-02 12:20:34 -0500

    complete

--- index.js
+++ index.js
@@ -10,9 +10,11 @@ function drawSymbol(pos) {
         : (player = 2);
     if (player == 1) {
         document.getElementById(pos.target.id).innerHTML = "X";
+        document.getElementById(pos.target.id).disabled = true;
         document.getElementById("turn").innerHTML = "Turno de Jugador 2";
     } else {
         document.getElementById(pos.target.id).innerHTML = "O";
+        document.getElementById(pos.target.id).disabled = true;
         document.getElementById("turn").innerHTML = "Turno de Jugador 1";
     }
     gameArray[pos.target.id[0]][pos.target.id[1]] = player;
@@ -48,19 +50,22 @@ function checkBoard(player) {
     )
         anyOneWon = 1;
     if (anyOneWon == 1) {
-        console.log(`Gana Jugador ${player}`);
+        document.getElementById("winner").innerHTML = `Gana Jugador ${player}`;
+        document.getElementById("winner").style.fontSize = "50px";
     }
 }
 function reset() {
     let buttons = document.getElementById("container").children;
     for (const iterator of buttons) {
         iterator.firstChild.innerHTML = "";
+        iterator.firstChild.disabled = false;
     }
     gameArray.forEach((item) => {
-        item.forEach(function (number) {
-            number = 0;
-        });
+        item.fill(0);
     });
+    document.getElementById("turn").innerHTML = "Turno de Jugador 1";
+    document.getElementById("winner").innerHTML = "";
+    document.getElementById("winner").style.fontSize = "10px";
 }
 
 window.addEventListener("load", () => {
